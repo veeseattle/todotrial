2d295f3139b6f626adf202e5cac2b90bcf826f3d
veeseattle <v-vakurn@microsoft.com>
2016-04-11 14:51:26 -0700

    changed config file to include docdb keys

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.host = process.env.HOST || "[URI for DocumentDB endpoint]";
-config.authKey = process.env.AUTH_KEY || "[Master key for DocumentDB]";
+config.host = process.env.HOST || "https://vsts.documents.azure.com:443/";
+config.authKey = process.env.AUTH_KEY || "ffiVDzFYRzObZmvtW3Int3DqrQL32M8eDJOmbLNMXnaAyEvXBN8BoYxH7emdE2gbLJwzCsfbNilwVai5E2GgEmw==";
 config.databaseId = "ToDoList";
 config.collectionId = "Items";
 
